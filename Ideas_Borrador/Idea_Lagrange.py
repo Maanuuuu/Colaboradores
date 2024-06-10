576e8742f76c5b8a58d2d53cb02ea7296416ed33
Juan Wu <juanwu8238@gmail.com>
2024-06-10 16:11:16 -0400

    Idea sobre Interpolacion de Lagrange en py: Importar Librerias necesarios y datos

---
+++ Ideas_Borrador/Idea_Lagrange.py
@@ -0,0 +1,7 @@
+#Importar la libreria
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Datos de prueba
+baja = np.array([10,7,4,3,5,2,2]) #Temperatura baja
+alta = np.array([21,18,12,11,14,13,10]) #Temperatura alta
