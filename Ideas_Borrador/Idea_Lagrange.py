a1f006b2b4d6c6640354c97c53de5238e32fd3e6
Juan Wu <juanwu8238@gmail.com>
2024-06-10 16:16:11 -0400

    Inicializacion de variables

--- Ideas_Borrador/Idea_Lagrange.py
+++ Ideas_Borrador/Idea_Lagrange.py
@@ -5,3 +5,18 @@ import matplotlib.pyplot as plt
 #Datos de prueba
 baja = np.array([10,7,4,3,5,2,2]) #Temperatura baja
 alta = np.array([21,18,12,11,14,13,10]) #Temperatura alta
+
+#Variables para trabajar:
+dias = len(baja) #Esto es para contar cuanto dias
+promedio_T = np.zeros(dias)
+dias_lista = np.zeros(dias)
+
+#Sacar promedio de temperatura
+for i in range (dias):
+    promedio_T[i]= (alta[i]+baja[i])/2
+
+#Contar cantidad de dias
+j = 1
+for i in range (len(promedio_T)):
+    dias_lista[i] = j
+    j += 1
